cecde8793c17a87d0f700a08f0a714a000a3c218
Jinmo Chong <jinmo55555@gmail.com>
2017-03-19 08:18:30 -0400

    fixed requirements and comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,47 +1,60 @@
 import fresh_tomatoes
 import media
 
+# instance of movie underworld
 underworld5 = media.Movie("underworld_bloodwars",
                           "A story of werewolves and vampires",
-                          "http://cdn1-www.comingsoon.net/assets/uploads/gallery/underworld-blood-wars/underworldbwposter.jpg",
+                          "http://cdn1-www.comingsoon.net/"  
+			  + "assets/uploads/gallery/underworld-blood-wars/underworldbwposter.jpg",
                           "https://www.youtube.com/watch?v=rKHL5PyAPzs"
                           )
 
+# instance of movie source code
 source_code = media.Movie("source code",
                           "time travel by programming",
-                          "https://upload.wikimedia.org/wikipedia/en/e/e5/Source_Code_Poster.jpg",
+                          "https://upload.wikimedia.org/" 
+			  + "wikipedia/en/e/e5/Source_Code_Poster.jpg",
                           "https://www.youtube.com/watch?v=DiBVUulE_wo"
                           )
 
-
+# instance of movie Moana
 Moana = media.Movie("Moana",
                     "A story of indian supernatural",
-                    "https://lumiere-a.akamaihd.net/v1/images/open-uri20160812-3094-p8x4dd_05e53f81.jpeg?region=0%2C0%2C1536%2C664",
+                    "https://lumiere-a.akamaihd.net/v1/images/" 
+		    + "open-uri20160812-3094-p8x4dd_05e53f81.jpeg?region=0%2C0%2C1536%2C664",
                     "https://www.youtube.com/watch?v=LKFuXETZUsI"
                     )
 
+# instance of movie secret life of walter mitty
 walter_mitty = media.Movie("secret life of walter mitty",
                            "Daydreamer's unexpected short adventure for responsibility",
-                           "http://lifeteen.com/wp-content/uploads/2013/12/2013-12_LT-WalterMitty1.jpg",
+                           "http://lifeteen.com/wp-content/" 
+			   + "uploads/2013/12/2013-12_LT-WalterMitty1.jpg",
                            "https://www.youtube.com/watch?v=HddkucqSzSM"
                            )
 
+# instance of movie 50 first dates
 fifty_first_date = media.Movie("50 first dates",
                             "How to get an Amnesia girl",
-                            "http://cdn2-www.craveonline.com/assets/uploads/2014/02/50-First-Dates-10-Years-Later.jpg",
+                            "http://cdn2-www.craveonline.com/assets/uploads/" 
+			    + "2014/02/50-First-Dates-10-Years-Later.jpg",
                             "https://www.youtube.com/watch?v=ErjP5xMTc8I"
                             )
 
+# instance of movie edge of tomorrow
 edge_of_tomorrow = media.Movie("Edge of Tomorrow",
                                "time traveler winning a war against aliens",
-                               "https://upload.wikimedia.org/wikipedia/en/f/f9/Edge_of_Tomorrow_Poster.jpg",
+                               "https://upload.wikimedia.org/wikipedia/en/" 
+			       + "f/f9/Edge_of_Tomorrow_Poster.jpg",
                                "https://www.youtube.com/watch?v=yUmSVcttXnI"
                                )
 
-
-
-movies = [underworld5, source_code, Moana, walter_mitty, fifty_first_date, edge_of_tomorrow]
+# array of instances of the movies that i like
+movies = [underworld5, source_code, Moana, walter_mitty, 
+	  fifty_first_date, edge_of_tomorrow]
+# opening the page
 fresh_tomatoes.open_movies_page(movies)
 
+# test lines commented
 #print(Moana.storyline)
 #toy_story.show_trailer()
--- media.py
+++ media.py
@@ -1,16 +1,30 @@
 import webbrowser
 
 class Movie():
-    """This provides movie information"""
+    """This provides movie information
+       
+       Attributes:
+       class variables about the movie ratings
+
+    """
     
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     
     def __init__(self, movie_title, movie_storyline,
                  poster_image, trailer_youtube):
+	"""
+	    constructor of the movie instance 
+	    with movie title, storyline, image, and youtube
+	    returns nothing.
+	"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+	"""
+	    this method shows nothing but takes you 
+	    to the url link which opens the youtube for trailer
+	"""
         webbrowser.open(self.trailer_youtube_url)
